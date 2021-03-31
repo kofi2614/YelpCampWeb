38878b6fcbd09efb6e09a3ef9fadcce66fd7e509
kofi2614 <kofiwang2614@gmail.com>
2021-03-31 00:36:57 -0400

    setting up middleware

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
+const morgan = require("morgan");
 const methodOverride = require("method-override");
 const Campground = require("./models/campground");
 
@@ -21,6 +22,8 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+
+app.use(morgan("tiny"));
 
 // home page
 app.get("/", (req, res) => {
