8bdbbb50bc430ae306620cebd2597abed76ae43a
Kofi Wang <kofiwang2614@gmail.com>
2021-04-25 21:42:02 -0400

    fixed error in campground model

--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Review = require("./review");
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
@@ -9,7 +10,7 @@ const CampgroundSchema = new Schema({
   location: String,
   reviews: [
     {
-      types: Schema.Types.ObjectId,
+      type: Schema.Types.ObjectId,
       ref: "Review",
     },
   ],
